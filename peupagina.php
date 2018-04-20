52b44766f554886d6a9d5d8dffc570516ac4e568
root <root@inf1-segabo.fjeclot.net>
2018-04-20 20:29:33 +0200

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/segabo1718daw2/pluginwp2_sergar
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Sergi Garcia Botella
+Author URI: https://github.com/segabo1718daw2
+License: GNU GPLv3
+*/
+?>
