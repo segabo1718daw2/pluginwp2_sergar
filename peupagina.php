c9ee364dbeed128928f9196a802e4c18a0a432ff
root <root@inf1-segabo.fjeclot.net>
2018-04-23 20:52:26 +0200

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -8,4 +8,12 @@ Author: Sergi Garcia Botella
 Author URI: https://github.com/segabo1718daw2
 License: GNU GPLv3
 */
+function admin_menu_peu_pagina(){
+$titolPagina="Peu de pàgina per defecte";
+$titolEntrada="Peu de pàgina per defecte";
+$role="administrator";
+$id="peu_pagina_per_defecte";
+add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
